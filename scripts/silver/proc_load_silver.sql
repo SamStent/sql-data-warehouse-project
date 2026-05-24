a971cf772d311545c3c1ecac957a3d50831c5f99
SamStent <165384146+SamStent@users.noreply.github.com>
2026-05-24 22:21:09 +0100

    Insert data into multiple silver tables from bronze

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -131,3 +131,62 @@ SELECT
         ELSE sls_price
     END
 FROM bronze.crm_sales_details;
+
+
+
+-- ============================================================================
+-- 4) CARGA TABLA: silver.erp_cust_az12
+-- ============================================================================
+INSERT INTO silver.erp_cust_az12 (
+	cid,
+	bdate,
+	gen
+)
+SELECT
+	CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LENGTH(cid)) -- Quita el prefijo 'NAS' si existe
+		ELSE cid
+	END AS cid,
+	CASE WHEN bdate > CURRENT_TIMESTAMP THEN NULL -- Cambia nacimientos futuros a NULL
+		ELSE bdate
+	END AS bdate,
+	CASE WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+		 WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+		 ELSE 'n/a'
+	END AS gen
+FROM bronze.erp_cust_az12;
+
+
+
+-- ============================================================================
+-- 4) CARGA TABLA: silver.erp_loc_a101
+-- ============================================================================
+INSERT INTO silver.erp_loc_a101 (
+	cid,
+	cntry
+)
+SELECT
+	REPLACE(cid, '-', '') AS cid,
+	CASE WHEN UPPER(TRIM(cntry)) = 'DE' THEN 'Germany'
+		 WHEN UPPER(TRIM(cntry)) IN ('US', 'USA') THEN 'United States'
+		 WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+		 ELSE TRIM(cntry)
+	END AS cntry
+FROM bronze.erp_loc_a101;
+
+
+
+-- ============================================================================
+-- 4) CARGA TABLA: silver.erp_px_cat_g1v2
+-- ============================================================================
+INSERT INTO silver.erp_px_cat_g1v2 (
+	id,
+	cat,
+	subcat,
+	maintenance
+)
+SELECT
+	id,
+	cat,
+	subcat,
+	maintenance
+FROM bronze.erp_px_cat_g1v2;
